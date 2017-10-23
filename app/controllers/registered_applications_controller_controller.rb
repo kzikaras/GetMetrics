e52172420dcb00229d3b2523154e562b02993b1a
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-10-22 21:46:39 -0400

    Build out views, functionality, and basic UI

--- app/controllers/registered_applications_controller_controller.rb
+++ app/controllers/registered_applications_controller_controller.rb
@@ -5,23 +5,32 @@ class RegisteredApplicationsControllerController < ApplicationController
   end
 
   def new
-    @application = Application.new
+    @application = Application.new 
   end
 
   def create
-    @application = Application.new
+    @application = Application.new 
     @application.name = params[:application][:name]
     @application.URL = params[:application][:URL]
+    @application.User_id = current_user.id
+    
+    Application.all.each do |name|
+      if name == @application.name
+        flash[:notice] = "Application already exists!"
+        render :new
+      end
+    end
 
     if @application.save
       flash[:notice] = "App was saved!"
-      redirect_to @application
+      redirect_to root_path
+    
     else
       flash.now[:notice] = "There was an error saving your app."
       render :new
     end
-
   end
+  
 
   def show
     @application = Application.find(params[:id])
@@ -31,5 +40,15 @@ class RegisteredApplicationsControllerController < ApplicationController
   end
 
   def destroy
+    @application = Application.find(params[:id])
+
+    if @application.destroy
+      flash[:notice] = "\"#{@application.name}\" was deleted successfully"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error deleting the app."
+      render :show
+    end
   end
+
 end
