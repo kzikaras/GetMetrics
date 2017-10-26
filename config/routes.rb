12670ef63b943984a47cc75a8d1ad2fd1a132e5f
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-10-26 18:59:49 -0400

    push everything up until 10/26

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,9 @@ Rails.application.routes.draw do
 
   root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+              # #2
+    resources :events, only: [:create]
+  end
 end
